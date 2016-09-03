b2948e888a3508c0584b5015edc6d4a718c48186
ftc8390 <ftc8390@gmail.com>
2016-09-03 19:30:15 -0400

    Test commit "and 4"

--- TemplateOpMode_Iterative.java
+++ TemplateOpMode_Iterative.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 // Inserting names to test github integration
-// ftc8390 test 3
+// ftc8390 test 3 and 4
 
 
 @TeleOp(name = "Test OpMode", group = "")  // @Autonomous(...) is the other common choice
